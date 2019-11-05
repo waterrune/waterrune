7d3cbcbbad2c3db4202f09438e9fe35628d86aa0
waterrune <53439172+waterrune@users.noreply.github.com>
2019-11-05 21:41:15 +0800

    First commit

---
+++ TransportSuggestor/TransportSuggestor1/TransportSuggestor1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TransportSuggestor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the distance between your start and end in metres. Type numbers only");
+
+            string distance = Console.ReadLine();
+
+            int distanceInt = Convert.ToInt32(distance);
+
+            if (distanceInt >= 100)
+            {
+                Console.WriteLine("Bus");
+            }
+            else
+            {
+                Console.WriteLine("Walk");
+            }
+        }
+    }
+}
