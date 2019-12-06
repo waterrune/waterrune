042bdb89f645910279f12287d84379103ee99793
waterrune <53439172+waterrune@users.noreply.github.com>
2019-12-06 22:18:18 +0800

    Initial lesson2

---
+++ Eric's Lesson 2 Functions and Methods/Eric's Lesson 2 Functions and Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Eric_s_Lesson_2_Functions_and_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+
+            Console.WriteLine("Hi");
+            Console.WriteLine("Hi");
+            Console.WriteLine("Hi");
+        }
+    }
+}
