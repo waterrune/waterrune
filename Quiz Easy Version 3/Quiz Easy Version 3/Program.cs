d30a83dc3cdce1b29a946cbf547aef4c3e8850d9
waterrune <53439172+waterrune@users.noreply.github.com>
2019-12-19 00:23:56 +0800

    this commit 3 is for version 3, for loops within for loops. limited tries.

--- Quiz Easy Version 3/Quiz Easy Version 3/Program.cs
+++ Quiz Easy Version 3/Quiz Easy Version 3/Program.cs
@@ -6,112 +6,139 @@ namespace Quiz_Easy_Version_3
     {
         static void Main(string[] args)
 
+
         {
-            Boolean forever = true;
-            while (forever)
+            //start of loop 1 for question 1
+            int i = 0;
+
+            for (; ; )
             {
+                if (i < 4)
 
-                Console.WriteLine("Welcome User. We are going to test you 3 questions. Answer correctly to proceed to next question." + '\n' + "Press 'a', 'b', or 'c'. " + '\n');
-                Console.WriteLine("Question 1: Which is red?" + '\n' + "a) Apple" + '\n' + "b) Banana" + '\n' + "c) Cranberry");
-
-                var ans = Console.ReadLine();
-
-
-
-                if (ans == "a")
                 {
-                    Console.WriteLine("Correct!");
-                    break;
+                    Console.WriteLine("Welcome User. We are going to test you 3 questions. Answer correctly to proceed to next question." + '\n' + "Press 'a', 'b', or 'c'. " + '\n');
+                    Console.WriteLine("Question 1: Which is red?" + '\n' + "a) Apple" + '\n' + "b) Banana" + '\n' + "c) Cranberry");
+
+                    var ans = Console.ReadLine();
+                    i++;
+
+
+                    if (ans == "a")
+                    {
+                        Console.WriteLine("Correct!");
+                        //start of loop 2 for question 2
+                        int i2 = 0;
+                        for (; ; )
+                        {
+                            if (i2 < 4)
+                                Console.WriteLine("Question 2: Which is blue?" + '\n' + "a) Water" + '\n' + "b) Horseshoe Crab Blood" + '\n' + "c) Oats");
+                            var ans2 = Console.ReadLine();
+                            i2++;
+                            if (ans2 == "b")
+
+                            {
+                                Console.WriteLine("Correct!");
+                                //start of loop 3 for question 3
+                                int i3 = 0;
+                                for (; ; )
+                                {
+                                    if (i3 < 4)
+                                        Console.WriteLine("Question 3: Which is purple?" + '\n' + "a) Mr Bean" + '\n' + "b) You" + '\n' + "c) Ditto");
+                                    var ans3 = Console.ReadLine();
+                                    i3++;
+                                    if (ans3 == "c")
+
+                                    {
+                                        Console.WriteLine("Correct! Congratulations! You've made it to the end! Score: 3/3");
+                                        Environment.Exit(0);
+                                    }
+                                    else if (ans3 == "a")
+                                    {
+
+                                        Console.WriteLine("Wrong!");
+                                        if (i3 == 4)
+                                            break;
+                                    }
+
+                                    else if (ans3 == "b")
+                                    {
+                                        Console.WriteLine("Wrong!");
+                                        if (i3 == 4)
+                                            break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("You have entered an invalid key! Please enter again");
+                                        if (i3 == 4)
+                                            break;
+                                    }
+                                }
+                                Console.WriteLine("You lose!");
+                                Environment.Exit(0);
+                            }
+                            else if (ans2 == "a")
+                            {
+                                Console.WriteLine("Wrong!");
+                                if (i2 == 4)
+                                    break;
+
+                            }
+                            else if (ans2 == "c")
+                            {
+                                Console.WriteLine("Wrong!");
+                                if (i2 == 4)
+                                    break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("You have entered an invalid key! Please enter again");
+                                if (i2 == 4)
+                                    break;
+                            }
+                        }
+                        Console.WriteLine("You lose!");
+                        Environment.Exit(0);
+
+                    }
+
+
+                    else if (ans == "b")
+                    {
+                        Console.WriteLine("Wrong!");
+                        if (i == 4)
+                            break;
+
+                    }
+
+
+
+                    else if (ans == "c")
+                    {
+
+                        Console.WriteLine("Wrong!");
+                        if (i == 4)
+                            break;
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("You have entered an invalid key! Please enter again");
+                        if (i == 4)
+                            break;
+
+                    }
                 }
-
-                else if (ans == "b")
-                {
-                    Console.WriteLine("Wrong!");
-
-                }
-                else if (ans == "c")
-                {
-                    Console.WriteLine("Wrong!");
-
-                }
-                else
-                {
-                    Console.WriteLine("You have entered an invalid key! Please enter again");
-
-                }
-
-
-
 
             }
-            bool forever2 = true;
-            while (forever2)
-
-            {
-
-                Console.WriteLine("Question 2: Which is blue?" + '\n' + "a) Water" + '\n' + "b) Horseshoe Crab Blood" + '\n' + "c) Oats");
-                var ans2 = Console.ReadLine();
-
-                if (ans2 == "b")
-
-
-                {
-                    Console.WriteLine("Correct!");
-
-                    break;
-                }
-                else if (ans2 == "a")
-                {
-                    Console.WriteLine("Wrong!");
-
-                }
-                else if (ans2 == "c")
-                {
-                    Console.WriteLine("Wrong!");
-
-                }
-                else
-                {
-                    Console.WriteLine("You have entered an invalid key! Please enter again");
-
-                }
-
-
-            }
-            Boolean forever3 = true;
-
-            while (forever3)
-            {
-
-
-                Console.WriteLine("Question 3: Which is purple?" + '\n' + "a) Mr Bean" + '\n' + "b) You" + '\n' + "c) Ditto");
-                var ans3 = Console.ReadLine();
-                if (ans3 == "c")
+            Console.WriteLine("You lose!");
+            Environment.Exit(0);
 
 
 
-                {
-                    Console.WriteLine("Correct! Congratulations! You've made it to the end! Score: 3/3");
-                    break;
 
-                }
-                else if (ans3 == "a")
-                {
-                    Console.WriteLine("Wrong!");
 
-                }
-                else if (ans3 == "b")
-                {
-                    Console.WriteLine("Wrong!");
 
-                }
-                else
-                {
-                    Console.WriteLine("You have entered an invalid key! Please enter again");
-
-                }
-
-            }
         }
     }
 }
