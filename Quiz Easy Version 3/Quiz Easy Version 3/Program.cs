a24854ad9e75c17315de0bc88ce23e64f0835db5
waterrune <53439172+waterrune@users.noreply.github.com>
2019-12-18 23:51:53 +0800

    first commit for quiz version 3 for loops within for loops

---
+++ Quiz Easy Version 3/Quiz Easy Version 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quiz_Easy_Version_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
